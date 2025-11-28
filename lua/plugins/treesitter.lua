302e12f17eb0c5df4f3fc3666168e913985b4308
Eden <azurehound.landline090@slmail.me>
2025-11-28 13:42:18 +0530

    ✨ feat(treesitter): Added kitty.conf support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,5 +13,25 @@ return {
         lint_events = { "BufWrite", "CursorHold" },
       },
     },
+
+    init = function()
+      vim.filetype.add({
+        pattern = {
+          [".*kitty%.conf"] = "kitty",
+          [".*/kitty/.*%.conf"] = "kitty",
+        },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          require("nvim-treesitter.parsers").kitty = {
+            install_info = {
+              url = "https://github.com/OXY2DEV/tree-sitter-kitty",
+            },
+          }
+        end,
+      })
+    end,
   },
 }
